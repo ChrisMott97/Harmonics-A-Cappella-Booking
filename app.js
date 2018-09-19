d252bf825411177897f0b032c06577489f000bd6
Chris <christopheramott@sky.com>
2018-09-19 23:42:07 +0100

    Updated a lot
    Added auth and new auditions times

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var fresherRouter = require('./routes/fresher');
+var userTable = require('./routes/models/user');
 
 var app = express();
 
@@ -24,10 +28,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://harmonics:harmonicsex1@ds131800.mlab.com:31800/harmonics';
+
+if(process.env.DATABASE_URL){
+  // var mongoDB = 'mongodb://harmonics:harmonicsex1@ds131800.mlab.com:31800/harmonics';
+  var mongoDB = process.env.DATABASE_URL
+}else{
+  var mongoDB = 'mongodb://harmonics:harmonicsex1@localhost/harmonics'
+}
+
 mongoose.connect(mongoDB);
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
@@ -36,6 +48,33 @@ var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+passport.use(new Strategy({
+  usernameField: 'username',
+  passwordField: 'password'
+},
+  function(username, password, cb) {
+    userTable.findOne({'username': username}, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+}));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  userTable.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
